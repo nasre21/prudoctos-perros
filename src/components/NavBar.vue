a2315c8071b350ba05968e61b9fa0819a504ea34
nasre21 <nasou2001@gmail.com>
2023-06-25 13:22:23 +0200

    tfinish task

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,23 +1,17 @@
 <template>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
+        <li class="nav-item" v-for='nav in navBar' :key='nav.title'>
+          <router-link :to="nav.rout" class="nav-link">{{ nav.title }}</router-link>
         </li>
       </ul>
     </div>
@@ -26,6 +20,26 @@
 </template>
 
 <script setup>
+
+const navBar = [
+  {
+   
+    src1:require("@/assets/img/head_screen.png"),
+    rout:"/"
+  },
+  {
+    title:"Shop",
+    rout:"/shop"
+  },
+  {
+   title:"About us",
+    rout:"/about"
+  },
+  {
+    title:"Contact",
+    rout:"/contact"
+  },
+]
 
 </script>
 
