58598e075ad750cfd6ecfe7f8452d1f64968e5e5
nasre21 <nasou2001@gmail.com>
2023-07-05 16:34:03 +0200

    add icons

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import router from './router'
 // import bootstrap
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+// import fonts
+import '@fortawesome/fontawesome-free/css/all.css';
 
 
 
