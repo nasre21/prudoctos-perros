470889684f86798eb647c6b6fd835ee5afbacb7b
nasre21 <nasou2001@gmail.com>
2023-07-10 15:21:45 +0200

    agregando footer

--- src/components/FooterView.vue
+++ src/components/FooterView.vue
@@ -10,10 +10,10 @@
     <!-- Left -->
 
     <!-- Right -->
-    <div v-for="icono in icons" :key="icono.id">
-<a :href="icono.rout" class="text-reset">
-  <i :class="icono.icon"></i>
-</a>
+    <div v-for="icon in icons" :key="icon.id">
+<router-link :to="icon.rout" class="text-reset">
+  <i :class="icon.icon"></i>
+</router-link>
     </div>
     <!-- Right -->
   </section>
@@ -28,11 +28,10 @@
         <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
           <!-- Content -->
           <h6 class="text-uppercase fw-bold mb-4">
-            <i class="fas fa-gem me-3"></i>Company name
+            <i class="fas fa-gem me-3"></i>About Us
           </h6>
           <p>
-            Here you can use rows and columns to organize your footer content. Lorem ipsum
-            dolor sit amet, consectetur adipisicing elit.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptatem eveniet eligendi quam repellendus!
           </p>
         </div>
         <!-- Grid column -->
@@ -41,7 +40,7 @@
         <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
           <!-- Links -->
           <h6 class="text-uppercase fw-bold mb-4">
-           About Us
+            Products
           </h6>
           <p>
             <a href="#!" class="text-reset">Angular</a>
@@ -83,17 +82,16 @@
         <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
           <!-- Links -->
           <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
-          <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+          <p><i class="fas fa-home me-3"></i> Spain, Bio 48004, SP</p>
           <p>
             <i class="fas fa-envelope me-3"></i>
-            info@example.com
+            mailto@mail.com
           </p>
-          <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
-          <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+          <p><i class="fas fa-phone me-3"></i> + 34 603 64 34 56</p>
         </div>
-        <!-- Grid column -->
+      
       </div>
-      <!-- Grid row -->
+      
     </div>
   </section>
   <!-- Section: Links  -->
@@ -109,7 +107,6 @@
 </template>
 
 <script setup>
-// iconos
 
 const icons = [{
   name: 'facebook',
@@ -142,13 +139,5 @@ const icons = [{
   rout:'https://es-es.facebook.com'
 }
   ]
-  // copyright
-
-  // const copyright = {
-  //   copy: '© 2021 Copyright: ',
-  //   href: 'https://www.tiendanimal.es/',
-  //   text: 'tiendanimal.es'
-
-  // }
 
 </script>
