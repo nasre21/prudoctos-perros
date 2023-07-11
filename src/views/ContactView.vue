2bf89ca4c7c36637438a4219e10173ef48b42888
nasre21 <nasou2001@gmail.com>
2023-07-11 15:37:14 +0200

    sharing beetwen view

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,8 +1,11 @@
 <template>
     <FormContact />
+    <NavBar />
+    <FootterView />
 </template>
 
 <script setup>
 import FormContact from '@/components/FormContact.vue';
-
+import NavBar from '@/components/NavBar.vue';
+  import FootterView from '@/components/FooterView.vue';
 </script>
